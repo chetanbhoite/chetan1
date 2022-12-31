2d20ee625a30447372b64f5a3c266363b7d2a40f
PC <PC@DESKTOP-2LMK3TO>
2022-12-31 09:10:21 +0000

    chetan module1

---
+++ src/sample2/demo1.java
@@ -0,0 +1,8 @@
+package sample2;
+
+public class demo1 {
+	public static void main(String[] args) {
+		System.out.println("good day");
+	}
+
+}
---
+++ src/sample2/demo2.java
@@ -0,0 +1,8 @@
+package sample2;
+
+public class demo2 {
+	public static void main(String[] args) {
+		System.out.println("nice to meet you");
+	}
+
+}
